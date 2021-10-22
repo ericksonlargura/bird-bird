7bd8edf52af18650734472a03f4848766528770b
ericksonlargura <erickson.largura@gmail.com>
2021-10-22 14:29:13 -0300

    change(Comment): Added script reference

--- scripts/ParallaxBackground.gd
+++ scripts/ParallaxBackground.gd
@@ -1,4 +1,6 @@
 extends ParallaxBackground
 
+# Função para movimentação do background
+# https://godotengine.org/qa/40998/how-to-make-an-infinite-scrolling-background
 func _process(delta):
 	scroll_offset.x -= 100 * delta
