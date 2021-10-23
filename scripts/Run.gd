3ce59738a45a2c40edbba4f4b4a80baf5d13551a
ericksonlargura <erickson.largura@gmail.com>
2021-10-23 02:13:40 -0300

    feat(Pipe Collision): Finally done

--- scripts/Run.gd
+++ scripts/Run.gd
@@ -1,8 +1,8 @@
 extends Node
 
 const PIPE_AFTER_SECONDS = 2
-const RND_MIN_PIPE = 400.0
-const RND_MAX_PIPE = 600.0
+const RND_MIN_PIPE = 360.0
+const RND_MAX_PIPE = 560.0
 const RND_MIN_SPACE = 80.0
 const RND_MAX_SPACE = 160.0
 const PIPE_HEIGHT = 320
@@ -12,6 +12,7 @@ const SPEED = 100
 var rng = RandomNumberGenerator.new()
 
 var sprite = preload('res://resources/Pipes.tscn')
+var point = preload('res://resources/Point.tscn')
 
 var pipes = []
 var seconds_passed = 0
@@ -39,21 +40,28 @@ func _process(delta):
 		top_pipe.rotation = PI
 		add_child(top_pipe)
 
+		var coin = point.instance()
+		coin.position = Vector2(VECTOR_X, y - (PIPE_HEIGHT / 2) - (space / 2))
+		add_child(coin)
+
 		pipes.append({
 			top = top_pipe,
-			bottom = bottom_pipe
+			bottom = bottom_pipe,
+			coin = coin
 		})
 
 	var should_remove = []
 	for i in range(pipes.size()):
-		if pipes[i].top.position.x < 0:
+		if pipes[i].top.position.x < -80:
 			pipes[i].top.queue_free()
 			pipes[i].bottom.queue_free()
+			pipes[i].coin.queue_free()
 			should_remove.append(i)
 		else:
 			var velocity = Vector2(delta * SPEED, 0)
 			pipes[i].top.position -= velocity
 			pipes[i].bottom.position -= velocity
-	
+			pipes[i].coin.position -= velocity
+
 	for i in should_remove:
 		pipes.remove(i)
