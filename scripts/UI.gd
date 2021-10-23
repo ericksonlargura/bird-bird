cc76bcd4d50f34d168916558d24e64064124fcc4
Gabriel Andreazza <k.gabriel.andreazza@gmail.com>
2021-10-23 02:14:28 -0300

    🦺

--- scripts/UI.gd
+++ scripts/UI.gd
@@ -3,17 +3,27 @@ extends Node2D
 onready var _game = get_node("/root/GameScript")
 
 func _ready():
+<<<<<<< Updated upstream
+=======
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	self.z_index = 5
+>>>>>>> Stashed changes
 	$TextureStart.visible = true
 	$GameOverUI.visible = false
+	$ButtonPlay.visible = false
 	get_tree().paused = true
 	z_index = 5
-
 
 func _process(delta):
 	if(_game.running):
 		$TextureStart.visible = false
-		$GameOverUI.visible = false	
+		$GameOverUI.visible = false
+		$ButtonPlay.visible = false
 		get_tree().paused = false
 	elif(_game.die):
 		$GameOverUI.visible = true
+		$ButtonPlay.visible = true
 		get_tree().paused = true
+	
+	print(get_global_mouse_position())
+	$Cursor.position = get_global_mouse_position() - Vector2(-12, -10)
