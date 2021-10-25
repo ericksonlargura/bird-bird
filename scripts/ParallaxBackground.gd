deac989ec79e049081172121fe74fb9021b8192c
ericksonlargura <erickson.largura@gmail.com>
2021-10-25 15:35:46 -0300

    feat: FileSystem structure

--- scripts/ParallaxBackground.gd
+++
@@ -1,8 +0,0 @@
-extends ParallaxBackground
-
-const SPEED = 100
-
-# Função para movimentação do background
-# https://godotengine.org/qa/40998/how-to-make-an-infinite-scrolling-background
-func _process(delta):
-	scroll_offset.x -= delta * SPEED
