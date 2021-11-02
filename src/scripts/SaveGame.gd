181b80eefb2411aed15842d4716c7fd7455c297c
ericksonlargura <erickson.largura@gmail.com>
2021-11-01 23:13:23 -0300

    feat(SaveGame): Improved filesystem

--- src/scripts/SaveGame.gd
+++ src/scripts/SaveGame.gd
@@ -8,14 +8,23 @@ func _ready():
 # Função para salvar o game
 # https://docs.godotengine.org/en/stable/tutorials/io/saving_games.html
 func save_game():
-	var save_dict = {
+	var date = OS.get_datetime()
+
+	var data = {
 		"points" : _game.points,
-		"time" : OS.get_datetime()
+		"time" : date
 	}
 
+	var directory = Directory.new()
+
+	if not directory.dir_exists('user://saves'):
+		directory.make_dir('user://saves')
+
+	var formatted_date = ("%s-%s-%s_%s.%s.%s" % [date['year'], date['month'], date['day'], date['hour'], date['minute'], date['second']])
+
 	var save_game = File.new()
-	save_game.open("user://savegame.save", File.WRITE)
-	save_game.store_line(to_json(save_dict))
+	save_game.open("user://saves/" + formatted_date + ".save", File.WRITE)
+	save_game.store_line(to_json(data))
 	save_game.close()
 
 func load_game():
@@ -24,6 +33,6 @@ func load_game():
 		return
 
 	save_game.open("user://savegame.save", File.READ)
-	var node_data = parse_json(save_game.get_line())
+	var _node_data = parse_json(save_game.get_line())
 
 	save_game.close()
